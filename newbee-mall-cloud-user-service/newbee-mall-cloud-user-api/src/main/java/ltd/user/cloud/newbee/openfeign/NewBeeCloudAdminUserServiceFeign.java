1809913b135019489b7ced56ffc7ae66ef13af1b
JordanCYX <2338257659@qq.com>
2023-12-02 15:34:50 +0800

    暴露接口返回类型修改

--- newbee-mall-cloud-user-service/newbee-mall-cloud-user-api/src/main/java/ltd/user/cloud/newbee/openfeign/NewBeeCloudAdminUserServiceFeign.java
+++ newbee-mall-cloud-user-service/newbee-mall-cloud-user-api/src/main/java/ltd/user/cloud/newbee/openfeign/NewBeeCloudAdminUserServiceFeign.java
@@ -1,5 +1,6 @@
 package ltd.user.cloud.newbee.openfeign;
 
+import ltd.common.cloud.newbee.dto.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,5 +9,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface NewBeeCloudAdminUserServiceFeign {
 
     @GetMapping(value = "/admin/{token}")
-    String getAdminUserByToken(@PathVariable(value = "token") String token);
+    Result getAdminUserByToken(@PathVariable(value = "token") String token);
 }
