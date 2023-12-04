85cb4dcf50b4aa612fe71a49cf024262049582d8
kallin.chen <kallin.chen@globrobot.com>
2023-12-04 15:44:46 +0800

    推荐管理模块初始化完成

---
+++ newbee-mall-cloud-recommend-service/newbee-mall-cloud-recommend-web/src/main/java/ltd/recommend/cloud/newbee/NewBeeMallCloudRecommendServiceApplication.java
@@ -0,0 +1,19 @@
+package ltd.recommend.cloud.newbee;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackageClasses = {ltd.user.cloud.newbee.openfeign.NewBeeCloudAdminUserServiceFeign.class})
+@MapperScan("ltd.recommend.cloud.newbee.dao")
+public class NewBeeMallCloudRecommendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBeeMallCloudRecommendServiceApplication.class, args);
+    }
+
+}
---
+++ newbee-mall-cloud-recommend-service/newbee-mall-cloud-recommend-web/src/main/java/ltd/recommend/cloud/newbee/entity/LoginAdminUser.java
@@ -0,0 +1,16 @@
+package ltd.recommend.cloud.newbee.entity;
+
+import lombok.Data;
+
+@Data
+public class LoginAdminUser {
+    private Long adminUserId;
+
+    private String loginUserName;
+
+    private String loginPassword;
+
+    private String nickName;
+
+    private Byte locked;
+}
